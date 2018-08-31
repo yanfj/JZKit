f5d18b1882b6d8127691b5f84b077dfeccffcc7c
Yan <yanfengjun@rrmj.tv>
2018-08-31 10:18:21 +0800

    add .h  .m

--- JZKit/JZCategory/UIKit/UITextField+JZExtension.h
+++ JZKit/JZCategory/UIKit/UITextField+JZExtension.h
@@ -8,5 +8,11 @@
 #import <UIKit/UIKit.h>
 
 @interface UITextField (JZExtension)
+/**
+ 设置长度限制,会自动截取到最大长度，已经处理Emoji的特殊情况
+
+ @param maxLength 最大长度
+ */
+- (void)jz_subTextToMaxLength:(NSInteger)maxLength;
 
 @end
